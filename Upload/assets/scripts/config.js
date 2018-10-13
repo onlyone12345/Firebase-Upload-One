d0f163fea22854036659e36200078295f90e0f05
Only-One <onlyfirdaus@gmail.com>
2018-10-13 20:22:33 +0700

    Revert "saya"
    This reverts commit e8c30fc67fd1e956519abcdc46d8aa4d251a3ca5.

--- Upload/assets/scripts/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyDa055c5bcXryLuYejhg7NGh4I8DUw6qkc",
-    authDomain: "awsom-80251.firebaseapp.com",
-    databaseURL: "https://awsom-80251.firebaseio.com",
-    projectId: "awsom-80251",
-    storageBucket: "awsom-80251.appspot.com",
-    messagingSenderId: "467553512378"
-};
-firebase.initializeApp(config);
